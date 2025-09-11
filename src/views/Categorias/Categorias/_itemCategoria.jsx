8b16db193b86244db39e2448548e2b10a7bb4d84
cris3p <cristianpineyro373@gmail.com>
2025-09-10 21:37:39 -0300

    asd

--- src/views/Categorias/Categorias/_itemCategoria.jsx
+++ src/views/Categorias/Categorias/_itemCategoria.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+const listaCategorias=[
+  {nombre:'Pizza', icon:'fa-pizza-circle'},
+  {nombre:'Hamburguesas', icon:'fa-hamburger'},
+  {nombre:'Pizza', icon:'fa-cocktail'},
+  {nombre:'Pizza', icon:'fa-ice-cream'},
+  {nombre:'Bebidas', icon:'fa-leaf'},
+  {nombre:'Sushi', icon:'fa-fish'},
+
+]
+
 const ItemCategoria = ({nombre}, {icono}) => {
   return (
     <div className='item-categoria'>
-        <div className='fa-solid fa-bowl-food'></div>
-        <span>Ramen del Chino</span>
+        <div className={`fa-solid ${icono}`}></div>
+        <span>{nombre}</span>
     </div>
   )
 }
