6e3ffbf85611e4c1b27b397c875b355a8a7a6c86
cris3p <cristianpineyro373@gmail.com>
2025-09-17 21:12:45 -0300

    l

--- src/views/Categorias/Categorias/Categorias.jsx
+++ src/views/Categorias/Categorias/Categorias.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import './Categorias.css'
-import ItemCategoria from './_itemCategoria'
+import React from 'react';
+import ItemCategoria from './_itemCategoria';
+
+const listaCategorias = [
+  { nombres: 'Pizza', icon: 'fas fa-pizza-slice' },  // Asegúrate de que 'icon' esté correcto
+  { nombres: 'Hamburguesas', icon: 'fas fa-hamburger' },
+  { nombres: 'Cócteles', icon: 'fas fa-cocktail' },
+  { nombres: 'Postres', icon: 'fas fa-ice-cream' },
+  { nombres: 'Bebidas', icon: 'fas fa-beer' },
+  { nombres: 'Sushi', icon: 'fas fa-fish' },
+];
 
 const Categorias = () => {
-  return (
-	<div className='categorias'>
-		<br></br>
-		<ItemCategoria />
-	</div>
-  );
-};
+	return (
+	  <div className="categorias">
+		{listaCategorias.map((item, index) => {
+		  console.log('Item en el map:', item); // Muestra el objeto 'item'
+		  return <ItemCategoria key={index} nombre={item.nombres} icono={item.icon} />;
+		})}
+	  </div>
+	);
+  };
+  
 
-export default Categorias
+export default Categorias;
--- src/views/Categorias/Categorias/_itemCategoria.jsx
+++ src/views/Categorias/Categorias/_itemCategoria.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
 
-const listaCategorias=[
-  {nombre:'Pizza', icon:'fa-pizza-circle'},
-  {nombre:'Hamburguesas', icon:'fa-hamburger'},
-  {nombre:'Pizza', icon:'fa-cocktail'},
-  {nombre:'Pizza', icon:'fa-ice-cream'},
-  {nombre:'Bebidas', icon:'fa-leaf'},
-  {nombre:'Sushi', icon:'fa-fish'},
-
-]
-
-const ItemCategoria = ({nombre}, {icono}) => {
+const ItemCategoria = ({ nombre, icono }) => {
   return (
     <div className='item-categoria'>
-        <div className={`fa-solid ${icono}`}></div>
-        <span>{nombre}</span>
+      <i className={`fa-solid ${icono}`}></i>
+      <span>{nombre}</span>
     </div>
   )
 }
 
 export default ItemCategoria
-
-// <div>
-// <div className='fa {iconoCategoria}'></div>
-// <span>{nombreCategoria}</span>
-// </div>
